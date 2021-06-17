7ceb10f97b6318f55a0b4574d2771bd266d2eed7
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-17 12:19:50 +0600

    Add files via upload
    Premetive Data Type

---
+++ learn.js
@@ -0,0 +1,28 @@
+// JS Premetive data Type
+
+
+// Integer 
+
+var int = 58;
+
+console.log("The Value is",int,"And the type is:",typeof( int));
+
+//String
+
+var string = "Rasel";
+
+console.log("The Value is",int,"And the type is:",typeof(string));
+
+//Undefined
+
+
+var Undefined = 
+
+console.log("The value is Undefined",typeof(Undefined));
+
+
+//Boolean
+
+var bool = true;
+
+console.log("The result is",typeof(bool));
